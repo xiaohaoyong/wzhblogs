33d3c66e1a6a47b30ea4691e5c9f117b9d69114f
wangzhen <wangzhen@xywy.com>
2016-10-08 19:03:09 +0800

    <?php $this->excerpt(150,'...'); ?>

--- usr/themes/wzhblogs/post.php
+++ usr/themes/wzhblogs/post.php
@@ -10,7 +10,7 @@
             <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
         </ul>
         <div class="post-content" itemprop="articleBody">
-            <?php $this->excerpt(500,'...'); ?>
+            <?php $this->content(); ?>
         </div>
         <p itemprop="keywords" class="tags"><?php _e('标签: '); ?><?php $this->tags(', ', true, 'none'); ?></p>
     </article>
